16946ab1b0e28055625c3fb2ebba5e3eb37d1357
sirgeo <CityMLT@yahoo.com>
2022-06-08 11:17:50 -0700

    deleted all sanitizer files

--- libs/arcgis-html-sanitizer/src/default.ts
+++
@@ -1,5 +0,0 @@
-import { Sanitizer } from "./index";
-
-// Rollup's UMD build will directly return the default export.
-// This is a workaround for Rollup not having a config property like Webpack's: libraryExport: 'Sanitizer'
-export default Sanitizer;
